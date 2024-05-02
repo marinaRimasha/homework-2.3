c4664995f97650c3e25d10693c19be06c2f5287f
marinaRimasha <rimasa.marina@gmail.com>
2024-05-02 23:45:26 +0300

    homework/2.3.1.2_fix

--- buttonDesc.js
+++ buttonDesc.js
@@ -7,7 +7,6 @@ class ChatButton {
       color,
     };
   }
-
   onClick() {
     console.log(this.props);
   }
@@ -16,6 +15,23 @@ class ChatButton {
 function testButton() {
   const sendMessageButton = new ChatButton(100, 40, "button", "green");
   sendMessageButton.onClick();
+  if (sendMessageButton instanceof ChatButton) {
+    console.log("sendMessageButton is an instance of ChatButton.");
+  } else {
+    console.log("sendMessageButton is not an instance of ChatButton.");
+  }
+
+  //verify we provided correct color
+  sendMessageButton.props.color === "green"
+    ? console.log("The button is actually green!")
+    : console.log(
+        "The button color is somehow colored " + sendMessageButton.props.color
+      );
+
+  //verify we provided all 4 properties
+  typeof sendMessageButton.props.width
+    ? console.log("Provided correct type for color width")
+    : console.log("Messed up with type for color width! ");
 }
 
 testButton();
