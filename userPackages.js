a17593b34a44083360f7e845e3a331e0a8ce1496
marinaRimasha <rimasa.marina@gmail.com>
2024-05-04 21:04:21 +0300

    homework/2.3.3/adjusted sms management

--- userPackages.js
+++ userPackages.js
@@ -1,10 +1,11 @@
 let userBalance = 500;
 let packageCost = 100;
-let smsCode = "A001DFX0";
+let sms = "посылка находится в ячейке код для получения A001DFX0";
+let smsCode = sms.split("получения ")[1]; //to get the code from the sms
 let cellID = 4;
 const postLockerDB = [null, null, null, "1432HGF", null];
 
-const packageInd = postLockerDB.findIndex(code => code === smsCode);
+const packageInd = postLockerDB.findIndex((code) => code === smsCode);
 if (packageInd != -1 && userBalance - packageCost >= 0) {
   userBalance -= packageCost;
   console.log(
